06677e31c403d00a34659f83191fcb92f78e6ee1
marmooo <marmooo@users.noreply.github.com>
2026-09-05 09:08:26 +0900

    0.3.3

--- build_npm.ts
+++ build_npm.ts
@@ -14,7 +14,7 @@ await build({
   },
   package: {
     name: "@marmooo/soundfont",
-    version: "0.3.2",
+    version: "0.3.3",
     description: "A SoundFont (SF2, SF3) parser and writer.",
     license: "MIT",
     repository: {
